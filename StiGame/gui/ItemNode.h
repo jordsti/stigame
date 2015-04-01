a52c525b68ed91e72e8ba6019f508c9bc83a25f2
jordsti <jord52@gmail.com>
2015-04-01 17:19:26 -0400

    little docs again

--- StiGame/gui/ItemNode.h
+++ StiGame/gui/ItemNode.h
@@ -8,20 +8,34 @@ namespace Gui {
 namespace _future {
 
 class ItemIterator;
-
+/// \class ItemNode
+/// \brief Forward Item Node
 class ItemNode {
 public:
+    /// \brief Create a new Node
+    /// \param m_item Item to put in the node
     ItemNode(Item *m_item);
+    /// \brief Destructor
     virtual ~ItemNode();
-
+    /// \brief Get the Item Pointer
+    /// \return Item Pointer
     Item* item(void);
+    /// \brief Get the next node pointer
+    /// \return Item Node Pointer
     ItemNode* next(void);
+    /// \brief Create a new Iterator with this node as starting node
+    /// \return New Iterator
     ItemIterator iterator(void);
-
+    /// \brief Set the next node
+    /// \param m_node Next Node
     void setNext(ItemNode *m_node);
+    /// \brief Is the Node contain a next element
+    /// \return Has Next element or not
     bool hasNext(void);
 private:
+    /// \brief Item Pointer
     Item *_item;
+    /// \brief Next Node
     ItemNode *_next;
 };
 
